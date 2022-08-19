e47772d605ef02e561682dae7a9ef1dd68973a19
Delphine <delphine.boitelleparsy@gmail.com>
2022-08-19 18:51:40 +0200

    test showcurrent final

--- gitDjango.sh
+++ gitDjango.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 #pour pouvoir exécuter le fichier bash, il faut le placer dans le répertoire de travail GIT ou le rendre global
-
 # récupérer la branche courrante et push
 current_branch=$(git branch --show-current)
 
 # git pull
 git pull origin "$current_branch"
-echo '\n'
 
 # récuperer le message passé en argument
 message="$1"
 
 # ajouter tous les changements
 git add .
-echo "> Tous les fichiers sont ajoutés. \n"
+echo "> Tous les fichiers sont ajoutés."
 
 # ajouter le commit avec le message
 git commit -m "$message"
-echo "> Commit ajouté avec le message: '$message' \n"
+echo "> Commit ajouté avec le message: '$message'"
 
 # git push
 git push origin "$current_branch"
